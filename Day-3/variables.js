169c18690b984179765a9a71681fcf59d771b3cc
SubsanMainali <subashmainali447@gmail.com>
2023-06-24 23:44:30 +0545

    Wrapping up my day's work.

---
+++ Day-3/variables.js
@@ -0,0 +1,16 @@
+var x = 52,
+    y = 10;
+x = x % y; // Remainder operator
+
+var a = 10,
+    b = 2;
+a = a ** b;
+a
+
+// Careful
+var z = '7';
+z = +z; // + operator made changed datatype to a number, int
+
+var g = true;
+g = +g; // + operator boolean to a number, int
+g
