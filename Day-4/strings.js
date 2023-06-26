161e931142533372f1c859c5f1b0c1af74625059
SubsanMainali <subashmainali447@gmail.com>
2023-06-26 15:39:53 +0545

    New string methods in js

--- Day-4/strings.js
+++ Day-4/strings.js
@@ -42,5 +42,24 @@ console.log(`Your name has ${nameLength} characters.`);
 var str = "Please \'locate\' where \'locate\' occurs. To \'locate\' means find something.";
 var fpos = str.indexOf('locate');
 var lpos = str.lastIndexOf("locate");
+console.log("The search space is \n", str);
 console.log(`The first position of locate is ${fpos}`);
 console.log(`The last postion of locate is ${lpos}`);
+
+var startpos = 15;
+var pos = str.indexOf('locate', startpos); // starting position of search
+var lpos = str.lastIndexOf('locate', startpos); // from end to beginning
+// given value becomes the end, and start searching towards beginning.
+console.log(`The first positon with starting at ${startpos} is ${pos}`);
+console.log(`The last positiong with starting at ${startpos} is ${lpos}`);
+
+var strpos = str.search('locate');
+console.log(`Search method, found position is ${strpos}`);
+
+// String slicing
+var fruitNames = "AppleBananaKiwi";
+var apple = fruitNames.slice(0, 5);
+var banana = fruitNames.slice(5, 11);
+console.log(`Names of fruit : ${fruitNames}`);
+console.log(`The first fruit is : ${apple}`);
+console.log(`The secong fruit name is : ${banana}`);
