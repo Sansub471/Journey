23fd3af42667189eeb8856268cabd70d5b9f2562
SubsanMainali <subashmainali447@gmail.com>
2023-06-28 13:43:43 +0545

    objects esercise completed, starting day 7.

--- Day-6/objects_exercise.js
+++ Day-6/objects_exercise.js
@@ -8,25 +8,35 @@ const product = {
 console.log(product);
 
 // Factory function
-function createProduct(name, code) {
+function createProduct(name, price, discount, code) {
     return {
         itenName: name,
-        price: 50,
-        discount: 20,
-        itemCode: code
+        price: price,
+        discount: discount,
+        itemCode: code,
+        discountAmount() {
+            return (discount * price) / 100;
+        }
     };
 }
 
-const product_1 = createProduct('Rose', 'R45g');
+const product_1 = createProduct('Rose', 120, 14, 'R45g');
+let amount = product_1.discountAmount();
 console.log(product_1);
+console.log(`The discount amount is ${amount}`);
 
 // Constructor function
-function Product(name, code) {
+function Product(name, price, discount, code) {
     this.itenName = name,
-        this.price = 50,
-        this.discount = 20,
-        this.code = code
+        this.price = price,
+        this.discount = discount,
+        this.code = code,
+        this.discountAmount = function() {
+            return (this.price * this.discount) / 100;
+        }
 };
 
-const product_2 = new Product('Pineapple', 'PN38f');
+const product_2 = new Product('Pineapple', 150, 7, 'PN38f');
+let disamt = product_2.discountAmount();
 console.log(product_2);
+console.log(`The discount amount is ${disamt}`);
