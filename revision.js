0c0c4b66a1a86a46cf8d7122dea90580e5e6a5b4
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-07-03 14:26:46 +0545

    The area of square functino usign set and get properties inside an object is done.

--- revision.js
+++ revision.js
@@ -4,3 +4,20 @@ let paragraph = "Support WebAssembly debugging" +
     "debugging with DWARF information (using" +
     "an extension)."
 let line = paragraph.slice(0, 20);
+
+// find area of a squre
+let square = {
+    set side(length) {
+        this.length = length;
+    },
+    get area() {
+        return this.length ** 2;
+    }
+
+}
+
+square.side = 10;
+console.log(`The area of square of side ${square.length} is ${square.area}`);
+
+square.side = 15;
+console.log(`The area of square of side ${square.length} is ${square.area}`);
