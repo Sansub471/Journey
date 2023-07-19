7a5435eb841cf7cf907215c493619fc526a464a8
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-07-19 23:48:52 +0545

    Light bulb project completed.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,5 +2,14 @@ let btn = document.getElementById('toggleBtn');
 btn.addEventListener('click', toggleBulb);
 
 function toggleBulb(e) {
-    bulb.src = "./images/lighton.jpg";
+    // Better to first check the current state of the light
+    if (btn.textContent.includes('On')) {
+        bulb.src = "./images/lighton.jpg";
+        btn.textContent = "Turn Off"
+    } else if (btn.textContent.includes('Off')) {
+        bulb.src = "./images/lightoff.jpg";
+        btn.textContent = "Tun On";
+    } else {
+        console.log("Error!");
+    }
 }
