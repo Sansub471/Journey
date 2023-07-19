e2fa288efb7bf41726e08f9f48c3985198eff7ef
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-07-19 23:59:36 +0545

    Done app.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -5,11 +5,11 @@ function toggleBulb(e) {
     // Better to first check the current state of the light
     if (btn.textContent.includes('On')) {
         bulb.src = "./images/lighton.jpg";
-        btn.textContent = "Turn Off"
+        btn.textContent = "Turn Off";
     } else if (btn.textContent.includes('Off')) {
         bulb.src = "./images/lightoff.jpg";
         btn.textContent = "Tun On";
     } else {
-        console.log("Error!");
+        console.log("Error Unexpected text content in the button!");
     }
 }
