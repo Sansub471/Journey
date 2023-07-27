659a08af43a0344c107d114854a17fbfc6657305
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-07-27 23:36:43 +0545

    create operation implemented.

--- app/scripts/courses.js
+++ app/scripts/courses.js
@@ -2,5 +2,15 @@ let addBtn = document.getElementById("add_btn");
 addBtn.addEventListener('click', addChapter);
 
 function addChapter(event) {
-    console.log("The add button clicked.")
+    let currentBtn = event.currentTarget;
+    let currentInput = currentBtn.previousElementSibling;
+    console.log(currentInput.value);
+
+    // Now add li 
+    let newLi = document.createElement('li');
+    newLi.classList.add('list-group-item');
+    newLi.textContent = currentInput.value;
+
+    let parentList = document.getElementById('parent-list');
+    parentList.appendChild(newLi);
 }
