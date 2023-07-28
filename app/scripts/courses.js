ab00223bd3cd5884a43c583c7cb7b631029b1e1d
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-07-28 22:58:44 +0545

    New style add feature done, using inner html.

--- app/scripts/courses.js
+++ app/scripts/courses.js
@@ -4,13 +4,19 @@ addBtn.addEventListener('click', addChapter);
 function addChapter(event) {
     let currentBtn = event.currentTarget;
     let currentInput = currentBtn.previousElementSibling;
-    //console.log(currentInput.value);
+    let currentChapterName = currentInput.value;
 
     // Now add li 
     let newLi = document.createElement('li');
-    newLi.classList.add('list-group-item'); // adding class to li
-    newLi.textContent = currentInput.value;
+    //newLi.classList.add('list-group-item'); // adding class to li
+    newLi.className = "list-group-item d-flex justify-content-between";
+    //newLi.textContent = currentInput.value;
+    // use inner html
+    newLi.innerHTML = `<h3 class="flex-grow-1">${currentChapterName}</h3>
+    <button class="btn btn-warning mx-3">Edit</button>
+    <button class="btn btn-danger">Remove</button>`
 
     let parentList = document.getElementById('parent-list');
     parentList.appendChild(newLi);
+
 }
