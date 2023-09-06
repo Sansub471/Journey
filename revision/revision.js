c0cfbf202a32feaed91969935ea91bb80b5fe065
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-06 22:18:14 +0545

    revision strings.

--- revision/revision.js
+++ revision/revision.js
@@ -3,3 +3,11 @@ function nameFunc() {
 }
 
 nameFunc();
+
+let string = 'This is a string';
+let another = 'You\'ve got no right to take away my plans.';
+console.log(another)
+
+// Template literals
+let finalString = `${string} ${another} \n Keep dreaming, and don't be dissappointed`;
+console.log(finalString)
