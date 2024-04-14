03fd835169128d62c39aa88ea7de28c25911dae3
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-04-14 22:24:24 +0545

    The second function is JS.

--- revision/functionsRevision.js
+++ revision/functionsRevision.js
@@ -14,3 +14,19 @@ function isPrime(num) {
 }
 
 // Formula : a * b = HCF(a, b) * LCM(a, b)
+function HCFLCM(a, b) {
+    let hcf = 1;
+    let c = a;
+    if (a > b) {
+        c = b;
+    }
+    for (let i = 1; i <= c; i++) {
+        if (a % i == 0 && b % i == 0) {
+            hcf = i;
+        }
+    }
+    console.log("The hcf is : ", hcf);
+    let lcm = (a * b) / hcf;
+    console.log("The LCM is : ", lcm);
+    return "The HCF is " + hcf + " and the LCM is " + lcm;
+}
